107f695e633497f5295f654a7c3b7a6093f3987d
Mary Cutrali <mary.cutrali@gmail.com>
2013-12-29 15:08:10 -0500

    first commit of directory

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Makeupbag::Application.config.secret_token = '519e549555189ff35c9678fd5f77c8431df2b5b70bd6b4c91226228d06a0163523549c4e497d61e118e872882a12691ed0acba2e0a93e66cc58b83f58443555a'
